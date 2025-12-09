2a264f76984a7de7b371d88a47b587300077f0ba
shivansh0536 <shivansh050906@gmail.com>
2025-12-10 01:42:24 +0530

    Fix Home page hero image URL

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -69,8 +69,8 @@ const Home = () => {
                 <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 bg-gray-100">
                     <img
                         className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-                        src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-                        alt="Delicious food spread"
+                        src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1760&q=80"
+                        alt="Delicious food bowl"
                     />
                     <div className="absolute inset-0 bg-gradient-to-r from-white to-transparent lg:via-white/20"></div>
                 </div>
